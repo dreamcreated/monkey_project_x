921c689322ae1b43f0726fe3d787607ca95ccc55
Li Da <zbwz@vip.qq.com>
2013-10-16 11:44:56 +0800

    add protobuf lib

--- set_environment.bat
+++ set_environment.bat
@@ -2,5 +2,6 @@ echo off
 setx MONKEY_HOME %cd%\monkey
 setx MONKEY_CLIENT_HOME %cd%\monkey_client
 setx MONKEY_COMMON %cd%\common
+setx PROTOBUF_HOME %cd%\protobuf
 echo 设置环境变量完成，请注销或重启系统使设置生效。
 pause
